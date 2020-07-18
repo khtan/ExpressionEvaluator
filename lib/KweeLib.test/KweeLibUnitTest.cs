16a39ef2807f7405b627645c2567b1baca510232
Kwee Tan <tan.k.h@juno.com>
2020-07-18 10:44:34 -0700

    Add unit test for KweeLib

---
+++ lib/KweeLib.test/KweeLibUnitTest.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using FluentAssertions;
+
+namespace KweeLib.test
+{
+    public class KweeLibUnitTest
+    {
+        private readonly ITestOutputHelper Output;
+        private readonly KweeCalc Calc;
+        public KweeLibUnitTest(ITestOutputHelper output)
+        {
+            Output = output;
+            Calc = new KweeCalc();
+        }
+        [Fact]
+        public void t0000_noSpaceBetweenParens()
+        {
+            var tuple = Calc.rationalizeExpression("(((3)))");
+            tuple.Item1.Should().Be("( ( ( ( 3 ) ) ) )"); // quirk : Should().Equal() is not what is seems
+            tuple.Item2.Should().BeNull();
+        }
+        [Fact]
+        public void t0001_unbalancedParens()
+        {
+            var tuple = Calc.rationalizeExpression(") 3 + 2 (");
+            tuple.Item1.Should().BeNull();
+            tuple.Item2.Should().Be("There are unbalanced parenthesis in the expression");
+        }
+        [Fact]
+        public void t0002_invalidCharacter()
+        {
+            var tuple = Calc.rationalizeExpression("3 x 2");
+            tuple.Item1.Should().BeNull();
+            tuple.Item2.Should().Be("There are invalid characters in the expression");
+        }
+    }
+}
