f9c145baed7dfc27548f3df633b7d1fcb1db3199
Kwee Tan <tan.k.h@juno.com>
2020-07-18 16:06:06 -0700

    Use fNNNN_description naming convention to show forwarded tests

--- functionaltest/FunctionalSuite1/KweeCalcTest.cs
+++ functionaltest/FunctionalSuite1/KweeCalcTest.cs
@@ -7,17 +7,41 @@ namespace FunctionalSuite1
 {
     public class KweeCalcTest : CommonCalcTest
     {
-    #region class internals
+        #region class internals
+        public ITestOutputHelper Output;
         public KweeCalcTest(ITestOutputHelper output) { 
             this.Output = output;
             this.CalcFn = Calc.CalcImplKwee;
         }
-    #endregion class internals
-    #region tests
+        #endregion class internals
+        #region tests
         [Fact]
         public void s0003_incompleteDecimalPass(){
             TestAnExpression("3.", 3);
         }
-    #endregion tests
+        #endregion special tests
+        #region forwarded tests
+        // Below are forwarding tests to the base class.
+        // A tool can be used to generate it
+        // Hence single line per test
+        [Theory]
+        [InlineData("2 + 3", 5)]
+        [InlineData("3 + 2", 5)]
+        [InlineData("2.0 + 3.0", 5.0)]
+        [InlineData("3.0 + 2.0", 5.0)]
+        public void f0000_Add(string expr, dynamic expectedResult) { t0000_Add(expr, expectedResult); }
+        [Theory]
+        [InlineData("2 * 3", 6)]
+        [InlineData("3 * 2", 6)]
+        [InlineData("2.0 * 3.0", 6.0)]
+        [InlineData("3.0 * 2.0", 6.0)]
+        public void f0000_Mult(string expr, dynamic expectedResult) { t0000_Mult(expr, expectedResult); }
+        [Fact] public void f0001_parenthesis() { t0001_parenthesis(); }
+        [Fact] public void f0003_decimal() { t0002_decimal(); }
+        [Fact] public void f0004_precedence() { t0004_precedence(); }
+        [Fact] public void f0005_precedence() { t0005_precedence(); }
+        [Fact] public void f0006_precedence() { t0006_precedence(); }
+        [Fact] public void f0007_precedence() { t0007_precedence(); }
+        #endregion forwarded tests
     }
 }
--- functionaltest/FunctionalSuite1/SpracheCalcTest.cs
+++ functionaltest/FunctionalSuite1/SpracheCalcTest.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using System;
 using Xunit;
 using Xunit.Abstractions;
 using FunctionalCalcLib;
@@ -7,17 +8,42 @@ namespace FunctionalSuite1
 {
     public class SpracheCalcTest : CommonCalcTest
     {
-    #region class internals
+        #region class internals
+        public ITestOutputHelper Output;
         public SpracheCalcTest(ITestOutputHelper output) { 
             this.Output = output;
             this.CalcFn = Calc.CalcImplSprache;
         }
     #endregion class internals
-    #region tests
+    #region special tests
         [Fact]
         public void s0003_incompleteDecimalFail(){
             TestAnExpression("3.", null, "Parsing failure: Unexpected end of input reached; expected numeric character (Line 1, Column 3); recently consumed: 3.");
         }
-    #endregion tests
+        #endregion special tests
+        #region forwarded tests
+        // Below are forwarding tests to the base class.
+        // A tool can be used to generate it
+        // Hence single line per test
+        [Theory]
+        [InlineData("2 + 3", 5)]
+        [InlineData("3 + 2", 5)]
+        [InlineData("2.0 + 3.0", 5.0)]
+        [InlineData("3.0 + 2.0", 5.0)]
+        public void f0000_Add(string expr, dynamic expectedResult) { t0000_Add(expr, expectedResult); }
+        [Theory]
+        [InlineData("2 * 3", 6)]
+        [InlineData("3 * 2", 6)]
+        [InlineData("2.0 * 3.0", 6.0)]
+        [InlineData("3.0 * 2.0", 6.0)]
+        public void f0000_Mult(string expr, dynamic expectedResult) { t0000_Mult(expr, expectedResult); }
+        [Fact] public void f0001_parenthesis() { t0001_parenthesis(); }
+        [Fact] public void f0003_decimal() { t0002_decimal(); }
+        [Fact] public void f0004_precedence() { t0004_precedence(); }
+        [Fact] public void f0005_precedence() { t0005_precedence(); }
+        [Fact] public void f0006_precedence() { t0006_precedence(); }
+
+        [Fact] public void f0007_precedence(){ t0007_precedence();}
+    #endregion forwarded tests
     }
 }
