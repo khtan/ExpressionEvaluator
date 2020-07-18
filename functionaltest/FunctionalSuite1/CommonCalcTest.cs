bcec5f027d3bb3e68a7712f0a49e9de22ad370ac
Kwee Tan <tan.k.h@juno.com>
2020-07-18 16:05:30 -0700

    Remove Xunit directives from CommonCalcTest.cs so that Test Runner does not misidentify them as tests

--- functionaltest/FunctionalSuite1/CommonCalcTest.cs
+++ functionaltest/FunctionalSuite1/CommonCalcTest.cs
@@ -1,20 +1,16 @@
 #nullable enable
 using System;
-using Xunit;
-using Xunit.Abstractions;
 using FluentAssertions;
 
 namespace FunctionalSuite1
 {
     public class CommonCalcTest
     {
-        public ITestOutputHelper Output;
         public Func<string, Tuple<dynamic?, string?>> CalcFn;
     #region testhelpers
         protected void TestAnExpression(string expr, dynamic? expected, string? errorMessage = null)
         {
             Tuple<dynamic?, string?> t = CalcFn(expr);
-            Output.WriteLine($"t={t}");
 /*
             var expectedTuple = new Tuple<dynamic?, string?>(expected, errorMessage);
             t.Should().Be(expectedTuple); // Bug? Be works in FunctionalCalcLibUnitTest but not here, always fail
@@ -33,49 +29,33 @@ namespace FunctionalSuite1
         }
         #endregion testhelpers
         #region tests
-        [Theory]
-        [InlineData("2 + 3", 5)]
-        [InlineData("3 + 2", 5)]
-        [InlineData("2.0 + 3.0", 5.0)]
-        [InlineData("3.0 + 2.0", 5.0)]
         public void t0000_Add(string expr, dynamic expectedResult)
         {
             TestAnExpression(expr, expectedResult);
         }
-        [Theory]
-        [InlineData("2 * 3", 6)]
-        [InlineData("3 * 2", 6)]
-        [InlineData("2.0 * 3.0", 6.0)]
-        [InlineData("3.0 * 2.0", 6.0)]
         public void t0000_Mult(string expr, dynamic expectedResult)
         {
             TestAnExpression(expr, expectedResult);
         }
-        [Fact]
         public void t0001_parenthesis(){
             TestAnExpression("(((3)))", 3);
         }
-        [Fact]
         public void t0002_decimal(){
             TestAnExpression(".3", 0.3);
         }
-        [Fact]
-        public void t004_precedence()
+        public void t0004_precedence()
         {
             TestAnExpression("2 + 3 * 5", 17);
         }
-        [Fact]
-        public void t005_precedence()
+        public void t0005_precedence()
         {
             TestAnExpression("2 * 3 + 5", 11);
         }
-        [Fact]
-        public void t006_precedence()
+        public void t0006_precedence()
         {
             TestAnExpression("2 + 3 * 5 + 7 * 9 + 11", 91);
         }
-        [Fact]
-        public void t007_precedence()
+        public void t0007_precedence()
         {
             TestAnExpression("2 * 3 + 5 * 7 + 9 * 11", 140);
         }
