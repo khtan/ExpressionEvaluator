e2e90bc28c8806a70b662a61db362ef4f0762a2c
Kwee Tan <tan.k.h@juno.com>
2020-07-18 16:44:43 -0700

    Add console runners for manual use, testing etc

---
+++ console/SpracheConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FunctionalCalcLib;
+
+namespace SpracheConsole
+{
+    /// <summary>
+    /// Interactive way to use/test calculator
+    /// Inputs are echoed with i:
+    /// Errors are echoed with e:
+    /// Values are echoed without any prefix indicators
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var codeToEval = Console.ReadLine();
+            Console.WriteLine($"i: {codeToEval}");
+            do
+            {
+                // use the functional interface for Sprache calculator
+                var t = Calc.CalcImplSprache(codeToEval);
+                if(t.Item1 != null) { // value
+                    Console.WriteLine(t.Item1);
+                }
+                if(t.Item2 != null) // error message
+                {
+                    Console.WriteLine($"error: {t.Item2}");
+                }
+                codeToEval = Console.ReadLine();
+                Console.WriteLine($"i: {codeToEval}");
+            } while (codeToEval != null);
+        }
+    }
+}
