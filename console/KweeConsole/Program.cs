5fd2539d445333d5edc7a2ad627172dd8a113ab5
Kwee Tan <tan.k.h@juno.com>
2020-07-18 17:07:43 -0700

    Added input files, modified do loop to avoid extra output

--- console/KweeConsole/Program.cs
+++ console/KweeConsole/Program.cs
@@ -4,19 +4,22 @@ using FunctionalCalcLib;
 namespace KweeConsole
 {
     /// <summary>
-    /// Interactive way to use/test calculator
-    /// Inputs are echoed with i:
-    /// Errors are echoed with e:
-    /// Values are echoed without any prefix indicators
+    /// Minimal console, without help etc
+    /// Provides interactive way to use/test calculator
+    ///    Inputs are echoed with i:
+    ///    Errors are echoed with e:
+    ///    Values are echoed without any prefix indicators
+    /// In a shell : enter Control-C to indicate end of input
+    /// In a file redirection, the EOF serves as end of input
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            var codeToEval = Console.ReadLine();
-            Console.WriteLine($"i: {codeToEval}");
-            do
+            string codeToEval;
+            while ((codeToEval = Console.ReadLine()) != null)
             {
+                Console.WriteLine($"i: {codeToEval}");
                 // use the functional interface for Kwee calculator
                 var t = Calc.CalcImplKwee(codeToEval);
                 if(t.Item1 != null) { // value
@@ -26,9 +29,7 @@ namespace KweeConsole
                 {
                     Console.WriteLine($"error: {t.Item2}");
                 }
-                codeToEval = Console.ReadLine();
-                Console.WriteLine($"i: {codeToEval}");
-            } while (codeToEval != null);
+            }
         }
     }
 }
