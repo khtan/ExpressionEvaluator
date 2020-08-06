1afb25aff4ae5cb650df06c73441d1c4cfceebb9
Kwee Tan <tan.k.h@juno.com>
2020-08-06 13:07:19 -0700

    -silent and -mode added

--- console/KweeConsole/Program.cs
+++ console/KweeConsole/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 
 namespace KweeConsole
 {
+    public enum Mode { ROBUST, FAST }
     /// <summary>
     /// Minimal console, without help etc
     /// Provides interactive way to use/test calculator
@@ -16,6 +17,34 @@ namespace KweeConsole
     /// In a shell : enter Control-C to indicate end of input
     /// In a file redirection, the EOF serves as end of input
     /// </summary>
+    [Command(ExtendedHelpText = @"
+Mode selection:
+   robust ( default ) : full checking of input, only allows well-formed expressions to be evaluated
+   fast : competitive programming where inputs are strictly well-formed
+Examples:
+   Interactive run
+      > KweeConsole.exe
+      > 1 + 2
+      > 3
+      > 2 + hello
+      > error1: There are invalid characters in the expression
+      >   C-c C-c^C
+
+   Regression run
+      > KweeConsole.exe -i test1.input -r test1.ref
+      > 19
+      > 40
+      > error3: 40 != 41
+      > 19
+      > 110
+      > Runtime: 00:00:00.08
+
+   Performance runs
+      > KweeConsole.exe -s -i explist.dat -m robust
+      Runtime: 00:00:07.86
+      > KweeConsole.exe -s -i explist.dat -m fast
+      Runtime: 00:00:01.37"
+    )] // appears at the end of help message
     class Program
     {
         // Properties for commandline option handling
@@ -23,21 +52,34 @@ namespace KweeConsole
         public string? InputFilePath { get; }
         [Option(Description = "(optional): Reference file to compare")]
         public string? RefFilePath { get; }
-        [Option(Description = "(optional) Performance run, turn off all outputs")]
-        public bool PerformanceOn { get; }
+        [Option(Description = "(optional) Silent by turning off all outputs")]
+        public bool Silent { get; }
+        [Option(Description = "(optional) Run mode , ie {fast, robust}")]
+        public Mode? ModeSetting { get; }
         // Internal properties
         public StreamReader? inputReader;
         public StreamReader? refReader;
+        public Mode mode;
+        public Func<string, Tuple<dynamic?, string?>>? CalcImpl;
         // Main
         static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
         // helpers
-        private void Setup(){
-            if(InputFilePath != null)
-            {
-                inputReader = new System.IO.StreamReader(InputFilePath);
-                System.Console.SetIn(inputReader);
+        private string? Setup(){
+            string? errorMessage = null;
+            try{
+                if(InputFilePath != null)
+                {
+                    inputReader = new System.IO.StreamReader(InputFilePath);
+                    System.Console.SetIn(inputReader);
+                }
+                if (RefFilePath != null) refReader = new System.IO.StreamReader(RefFilePath);
+                mode = ModeSetting ?? Mode.ROBUST;
+                if (mode == Mode.ROBUST) CalcImpl = Calc.CalcImplRobustKwee;
+                else CalcImpl = Calc.CalcImplFastKwee;
+            }catch(Exception ex){
+                errorMessage = ex.Message;
             }
-            if (RefFilePath != null) refReader = new System.IO.StreamReader(RefFilePath);
+            return errorMessage;
         }
         private void Teardown(){
             if (inputReader != null) inputReader.Dispose();
@@ -48,52 +90,64 @@ namespace KweeConsole
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
             Console.WriteLine($"Runtime: {elapsedTime}");
         }
-        private TimeSpan TimeReadExecute(bool isPerformance)
+        private TimeSpan TimeReadExecute(bool isSilent)
         { // A lot of lines to accomplish this, so keep in its own function to avoid distractions
             var stopWatch = new Stopwatch();
             stopWatch.Start();
-            ReadExecute(isPerformance); // measure this
+            if(isSilent){
+                ReadExecuteSilent(); // measure this
+            } else {
+                ReadExecuteVerbose();
+            }
             stopWatch.Stop();
             return stopWatch.Elapsed;
         }
-        private void ReadExecute(bool isPerformance){
+        private void ReadExecuteSilent(){
+            string codeToEval;
+            while ((codeToEval = Console.ReadLine()) != null)
+            {
+                var t = CalcImpl(codeToEval);
+            }//while
+        }
+        private void ReadExecuteVerbose(){
             string codeToEval;
             while ((codeToEval = Console.ReadLine()) != null)
             {
                 //  Console.WriteLine($"i: {codeToEval}");
                 // use the functional interface for Kwee calculator
-                var t = Calc.CalcImplRobustKwee(codeToEval);
-                if( isPerformance == false )
-                {
-                    if (t.Item1 != null) Console.WriteLine(t.Item1); // value
-                    if (t.Item2 != null) Console.WriteLine($"error1: {t.Item2}"); // error message
-                    if (refReader != null)
-                    {
-                        string? sd = refReader.ReadLine();
-                        if (!String.IsNullOrEmpty(sd))
+                    var t = CalcImpl(codeToEval);
+                        if (t.Item1 != null) Console.WriteLine(t.Item1); // value
+                        if (t.Item2 != null) Console.WriteLine($"error1: {t.Item2}"); // error message
+                        if (refReader != null)
                         {
-                            if (t.Item1 != null)
+                            string? sd = refReader.ReadLine();
+                            if (!String.IsNullOrEmpty(sd))
                             {
-                                Double d = Double.Parse(sd);
-                                if (d != t.Item1) Console.WriteLine($"error3: {t.Item1} != {d}");
+                                if (t.Item1 != null)
+                                {
+                                    Double d = Double.Parse(sd);
+                                    if (d != t.Item1) Console.WriteLine($"error3: {t.Item1} != {d}");
 
+                                }
+                                if (t.Item2 != null)
+                                {
+                                    if (sd != t.Item2) Console.WriteLine($"error4: {t.Item2} != {sd}");
+                                }
                             }
-                            if (t.Item2 != null)
-                            {
-                                if (sd != t.Item2) Console.WriteLine($"error4: {t.Item2} != {sd}");
-                            }
-                        }
-                        else Console.WriteLine($"error2: refFile out of inputs");
-                    }//if
-                }//if
+                            else Console.WriteLine($"error2: refFile out of inputs");
+                        }//if
             }//while
         }
         private void OnExecute()
         {
-            Setup();
-            var elapsed = TimeReadExecute(PerformanceOn);
-            PrintElapsedTime(elapsed);
-            Teardown();
+            var errorMessage = Setup();
+            if(errorMessage == null){
+                var elapsed = TimeReadExecute(Silent);
+                PrintElapsedTime(elapsed);
+                Teardown();
+            } else {
+                Console.WriteLine($"error5: {errorMessage}");
+            }
         }
     }
 }
